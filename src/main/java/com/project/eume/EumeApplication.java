8f68915bd1985d5e2faf2bf95ac6e31973ec4511
DancingScoop <leehh0221@gmail.com>
2025-11-26 14:48:29 +0900

    Add initial Spring Boot

---
+++ src/main/java/com/project/eume/EumeApplication.java
@@ -0,0 +1,13 @@
+package com.project.eume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EumeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EumeApplication.class, args);
+	}
+
+}
